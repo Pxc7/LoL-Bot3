37d7cf9fab9297f36bcd681aba72d3b92c820212
Pxc7 <81814182+Pxc7@users.noreply.github.com>
2021-04-06 17:35:36 +0700

    Add files via upload

---
+++ help/help.js
@@ -0,0 +1,490 @@
+const help = (prefix, pushname2, premi, uptime, tanggal, pepolu, hayuk ) => {
+    return `╭───「 INGFO 」 ​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+│
+├ •name   :  *${pushname2}*
+├ •user     :  ${premi}
+├ •on        :  ${kyun(uptime)}
+├ •tgl        :  *${tanggal}*
+├ •Prefix  :  *${prefix}*
+│
+├───「 *COMMAND* 」─────
+│
+├ • *${pepolu}* command
+│
+├───「 *TERDAFTAR* 」────
+│
+├ • *${hayuk}* Regis
+│
+├──「 MENU 」
+│
+├ • ${prefix}help
+├ • ${prefix}owner
+├ • ${prefix}donate
+├ • ${prefix}takestick
+├ • ${prefix}leave
+├ • ${prefix}delete
+├ • ${prefix}setapikey
+├ • ${prefix}ocr
+├ • ${prefix}sticker2 (no apikey)
+├ • ${prefix}tagstick
+├ • ${prefix}tagimg
+├ • ${prefix}toimg
+├ • ${prefix}togif
+├ • ${prefix}shortlink
+├ • ${prefix}setcr
+├ • ${prefix}setprefix
+├ • ${prefix}sticker
+├ • ${prefix}stickerwm
+├ • ${prefix}broadcast
+├ • ${prefix}addprem
+├ • ${prefix}dellprem
+├ • ${prefix}listprem
+├ • ${prefix}listpenyimak
+│
+├──「 Group 」
+├ • ${prefix}kick
+├ • ${prefix}add
+├ • ${prefix}listadmin
+├ • ${prefix}setname
+├ • ${prefix}setpp
+├ • ${prefix}antivirtek
+├ • ${prefix}antilink
+├ • ${prefix}demote
+├ • ${prefix}promote
+├ • ${prefix}grup buka/tutup
+│
+├──「 TOD 」
+├ • ${prefix}dare
+├ • ${prefix}truth
+│
+├──「 Islami 」
+├ • ${prefix}listsurah
+├ • ${prefix}asmaulhusna
+├ • ${prefix}alquran no_surah
+├ • ${prefix}alquranaudio no_surah
+├ • ${prefix}alquranaudio no_surah/no_ayat
+├ • ${prefix}alquranaudio no_surah/no_ayat1-no_ayat2
+├ • ${prefix}kisahnabi nama_nabi
+├ • ${prefix}jadwalsholat daerah
+│
+├──「 Downloader 」
+├ • ${prefix}ytsearch query
+├ • ${prefix}ytplay query
+├ • ${prefix}ytplay2 query
+├ • ${prefix}ytmp3 url_video
+├ • ${prefix}ytmp32 url_video
+├ • ${prefix}ytmp4 url_video
+├ • ${prefix}ytmp42 url_video
+├ • ${prefix}tiktoknowm url_video
+├ • ${prefix}tiktokmusic url_video
+├ • ${prefix}igdl url_post
+├ • ${prefix}fbdl url_video
+├ • ${prefix}jooxplay query
+├ • ${prefix}spotify url_music
+├ • ${prefix}spotifysearch query
+├ • ${prefix}pinterest query
+├ • ${prefix}pinterestdl url_pinterest
+├ • ${prefix}pixiv query
+├ • ${prefix}pixivdl url_pixiv
+├ • ${prefix}zippyshare url_zippyshare
+├ • ${prefix}telesticker url_pack
+│
+├──「 Movie & Story 」
+├ • ${prefix}drakorongoing
+├ • ${prefix}lk21 query
+├ • ${prefix}wattpad url_wattpad
+├ • ${prefix}wattpadsearch query
+├ • ${prefix}cerpen
+├ • ${prefix}ceritahoror
+│
+├──「 Searching 」
+├ • ${prefix}shopee query
+├ • ${prefix}gimage query
+├ • ${prefix}google query
+├ • ${prefix}gimage2 query
+├ • ${prefix}konachan query
+├ • ${prefix}playstore query
+├ • ${prefix}stickerwa query
+├ • ${prefix}wallpapersearch query
+├ • ${prefix}wallpapersearch2 query
+│
+├──「 Random Text 」
+├ • ${prefix}quotes
+├ • ${prefix}quotesdilan
+├ • ${prefix}quotesanime
+├ • ${prefix}quotesimage
+├ • ${prefix}faktaunik
+├ • ${prefix}katabijak
+├ • ${prefix}pantun
+├ • ${prefix}bucin
+├ • ${prefix}randomnama
+│
+├──「 AniManga 」
+├ • ${prefix}wait
+├ • ${prefix}quotesanime
+├ • ${prefix}manga query
+├ • ${prefix}anime query
+├ • ${prefix}character query
+├ • ${prefix}kusonime url_kusonime
+├ • ${prefix}kusonimesearch query
+├ • ${prefix}otakudesu url_otakudesu
+├ • ${prefix}otakudesusearch query
+├ • ${prefix}nhentai kode_bom
+├ • ${prefix}nhentaipdf kode_bom
+├ • ${prefix}nhentaisearch query
+├ • ${prefix}nekopoi url
+├ • ${prefix}nekopoisearch query
+│
+├──「 Information 」
+├ • ${prefix}jadwaltv channel
+├ • ${prefix}jadwaltvnow
+├ • ${prefix}jadwalbola
+├ • ${prefix}heroml hero_name
+├ • ${prefix}wikipedia query
+├ • ${prefix}translate kode_negara text
+├ • ${prefix}brainly query
+├ • ${prefix}newsinfo
+├ • ${prefix}cnnindonesia
+├ • ${prefix}cnnnasional
+├ • ${prefix}cnninternasional
+├ • ${prefix}infogempa
+├ • ${prefix}lirik query
+├ • ${prefix}cuaca daerah
+├ • ${prefix}kodepos query
+├ • ${prefix}indbeasiswa
+├ • ${prefix}hoax
+├ • ${prefix}nsfwcheck
+│
+├──「 Entertainment 」
+├ • ${prefix}tebakbendera
+├ • ${prefix}tebakgambar
+├ • ${prefix}family100
+├ • ${prefix}caklontong
+├ • ${prefix}wancak
+├ • ${prefix}asupan
+├ • ${prefix}meme
+├ • ${prefix}memeindo 
+├ • ${prefix}namaninja 
+├ • ${prefix}alay
+├ • ${prefix}hilih
+├ • ${prefix}purba
+├ • ${prefix}besarkecil
+│
+├──「 Creator 」
+├ • ${prefix}ttp
+├ • ${prefix}ttp2
+├ • ${prefix}ttp3
+├ • ${prefix}ttp4
+├ • ${prefix}attp
+├ • ${prefix}qrcode 
+├ • ${prefix}nulis
+├ • ${prefix}ytkomen
+├ • ${prefix}phkomen
+├ • ${prefix}amongus
+├ • ${prefix}emojitoimg
+├ • ${prefix}ktpmaker
+│
+├──「 Creator 」
+├ • ${prefix}ttp text
+├ • ${prefix}ttp2 text
+├ • ${prefix}ttp3 text
+├ • ${prefix}ttp4 text
+├ • ${prefix}attp text
+├ • ${prefix}smoji emoji
+├ • ${prefix}fakedonald text
+├ • ${prefix}ktpmaker
+│
+├──「 Primbon 」
+├ • ${prefix}artinama name
+├ • ${prefix}jodoh name1 & name2 
+├ • ${prefix}weton tanggal bulan tahun
+├ • ${prefix}jadian tanggal bulan tahun
+├ • ${prefix}tebakumur name
+│
+├──「 Other 」
+├ • ${prefix}spamsms nomor
+│
+├──「 Stalking 」
+├ • ${prefix}ytstalk
+├ • ${prefix}tkstalk
+├ • ${prefix}githubstalk
+├ • ${prefix}igstalk
+├ • ${prefix}twtstalk
+│
+├──「 Text Pro Me 」
+├ • ${prefix}blackpink text
+├ • ${prefix}neon text
+├ • ${prefix}greenneon text
+├ • ${prefix}advanceglow text
+├ • ${prefix}futureneon text
+├ • ${prefix}sandwriting text
+├ • ${prefix}sandsummer text
+├ • ${prefix}sandengraved text
+├ • ${prefix}metaldark text
+├ • ${prefix}neonlight text
+├ • ${prefix}holographic text
+├ • ${prefix}text1917 text
+├ • ${prefix}minion text
+├ • ${prefix}deluxesilver text
+├ • ${prefix}newyearcard text
+├ • ${prefix}bloodfrosted text
+├ • ${prefix}halloween text
+├ • ${prefix}jokerlogo text
+├ • ${prefix}fireworksparkle text
+├ • ${prefix}natureleaves text
+├ • ${prefix}bokeh text
+├ • ${prefix}toxic text
+├ • ${prefix}strawberry text
+├ • ${prefix}box3d text
+├ • ${prefix}roadwarning text
+├ • ${prefix}breakwall text
+├ • ${prefix}icecold text
+├ • ${prefix}luxury text
+├ • ${prefix}cloud text
+├ • ${prefix}summersand text
+├ • ${prefix}horrorblood text
+├ • ${prefix}thunder text
+├ • ${prefix}pornhub text1 text2
+├ • ${prefix}glitch text1 text2
+├ • ${prefix}avenger text1 text2
+├ • ${prefix}space text1 text2
+├ • ${prefix}ninjalogo text1 text2
+├ • ${prefix}marvelstudio text1 text2
+├ • ${prefix}lionlogo text1 text2
+├ • ${prefix}wolflogo text1 text2
+├ • ${prefix}steel3d text1 text2
+├ • ${prefix}wallgravity text1 text2
+│
+├──「 Photo Oxy 」
+├ • ${prefix}shadow text
+├ • ${prefix}cup text
+├ • ${prefix}cup1 text
+├ • ${prefix}romance text
+├ • ${prefix}smoke text
+├ • ${prefix}burnpaper text
+├ • ${prefix}lovemessage text
+├ • ${prefix}undergrass text
+├ • ${prefix}love text
+├ • ${prefix}coffe text
+├ • ${prefix}woodheart text
+├ • ${prefix}woodenboard text
+├ • ${prefix}summer3d text
+├ • ${prefix}wolfmetal text
+├ • ${prefix}nature3d text
+├ • ${prefix}underwater text
+├ • ${prefix}golderrose text
+├ • ${prefix}summernature text
+├ • ${prefix}letterleaves text
+├ • ${prefix}glowingneon text
+├ • ${prefix}fallleaves text
+├ • ${prefix}flamming text
+├ • ${prefix}harrypotter text
+├ • ${prefix}carvedwood text
+├ • ${prefix}tiktok text1 text2
+├ • ${prefix}arcade8bit text1 text2
+├ • ${prefix}battlefield4 text1 text2
+├ • ${prefix}pubg text1 text2
+│
+├──「 Ephoto 360 」
+├ • ${prefix}wetglass text
+├ • ${prefix}multicolor3d text
+├ • ${prefix}watercolor text
+├ • ${prefix}luxurygold text
+├ • ${prefix}galaxywallpaper text
+├ • ${prefix}lighttext text
+├ • ${prefix}beautifulflower text
+├ • ${prefix}puppycute text
+├ • ${prefix}royaltext text
+├ • ${prefix}heartshaped text
+├ • ${prefix}birthdaycake text
+├ • ${prefix}galaxystyle text
+├ • ${prefix}hologram3d text
+├ • ${prefix}greenneon text
+├ • ${prefix}glossychrome text
+├ • ${prefix}greenbush text
+├ • ${prefix}metallogo text
+├ • ${prefix}noeltext text
+├ • ${prefix}glittergold text
+├ • ${prefix}textcake text
+├ • ${prefix}starsnight text
+├ • ${prefix}wooden3d text
+├ • ${prefix}textbyname text
+├ • ${prefix}writegalacy text
+├ • ${prefix}galaxybat text
+├ • ${prefix}snow3d text
+├ • ${prefix}birthdayday text
+├ • ${prefix}goldplaybutton text
+├ • ${prefix}silverplaybutton text
+├ • ${prefix}freefire text
+│
+├──「 Random Image  」
+├ • ${prefix}art
+├ • ${prefix}bts
+├ • ${prefix}exo
+├ • ${prefix}elf
+├ • ${prefix}loli
+├ • ${prefix}neko
+├ • ${prefix}waifu
+├ • ${prefix}shota
+├ • ${prefix}husbu
+├ • ${prefix}sagiri
+├ • ${prefix}shinobu
+├ • ${prefix}megumin
+├ • ${prefix}wallnime
+├ • ${prefix}chiisaihentai
+├ • ${prefix}trap
+├ • ${prefix}blowjob
+├ • ${prefix}yaoi
+├ • ${prefix}ecchi
+├ • ${prefix}hentai
+├ • ${prefix}ahegao
+├ • ${prefix}hololewd
+├ • ${prefix}sideoppai
+├ • ${prefix}animefeets
+├ • ${prefix}animebooty
+├ • ${prefix}animethighss
+├ • ${prefix}hentaiparadise
+├ • ${prefix}animearmpits
+├ • ${prefix}hentaifemdom
+├ • ${prefix}lewdanimegirls
+├ • ${prefix}biganimetiddies
+├ • ${prefix}animebellybutton
+├ • ${prefix}hentai4everyone
+├ • ${prefix}bj
+├ • ${prefix}ero
+├ • ${prefix}cum
+├ • ${prefix}feet
+├ • ${prefix}yuri
+├ • ${prefix}trap
+├ • ${prefix}lewd
+├ • ${prefix}feed
+├ • ${prefix}eron
+├ • ${prefix}solo
+├ • ${prefix}gasm
+├ • ${prefix}poke
+├ • ${prefix}anal
+├ • ${prefix}holo
+├ • ${prefix}tits
+├ • ${prefix}kuni
+├ • ${prefix}kiss
+├ • ${prefix}erok
+├ • ${prefix}smug
+├ • ${prefix}baka
+├ • ${prefix}solog
+├ • ${prefix}feetg
+├ • ${prefix}lewdk
+├ • ${prefix}waifu
+├ • ${prefix}pussy
+├ • ${prefix}femdom
+├ • ${prefix}cuddle
+├ • ${prefix}hentai
+├ • ${prefix}eroyuri
+├ • ${prefix}cum_jpg
+├ • ${prefix}blowjob
+├ • ${prefix}erofeet
+├ • ${prefix}holoero
+├ • ${prefix}classic
+├ • ${prefix}erokemo
+├ • ${prefix}fox_girl
+├ • ${prefix}futanari
+├ • ${prefix}lewdkemo
+├ • ${prefix}wallpaper
+├ • ${prefix}pussy_jpg
+├ • ${prefix}kemonomimi
+├ • ${prefix}nsfw_avatar
+├ • ${prefix}ngif
+├ • ${prefix}nsfw_neko_gif
+├ • ${prefix}random_hentai_gif
+├─── ⸨ *TQTO* ⸩ 
+│
+│©LoL Human [mastah]
+│©Mr.A43G [waria]
+│©Sofyan [miring]
+│©Rizal [gay]
+│©LordAbdur [sensei]
+│©RBOT
+│©LoL TeaM
+│
+╰───「 LoL Api 」`
+}
+exports.help = help
+
+// penghitung aktif bot
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
+}
+
+const donate = (sender) => {
+    return `╭───「 donasi 」 ​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+│
+├ • Pulsa : 081462239208
+├ • Ovo : Coming soon
+├ • Dana : Coming soon
+│
+╰───「 LoL Api 」`
+}
+exports.donate = donate
+
+const bahasa = () => {
+    return `
+List Bahasa :
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.bahasa = bahasa
